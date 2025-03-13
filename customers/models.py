ebc03634b611a559b28c09d0690e274cdf439711
Pengyu <1205032562@qq.com>
2025-03-13 16:25:13 +0000

    model defined

---
+++ customers/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from cryptography.fernet import Fernet
+from django.conf import settings
+from django.contrib.auth.hashers import make_password, check_password
+import binascii
+import os
+# Create your models here.
+cipher =  Fernet(settings.FERNET_KEY.encode())
+
+class Customer(models.Model):
+    Customerid = models.AutoField(primary_key=True)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField(unique = True)
+    phone = models.CharField(max_length=15)
+    DOB = models.DateField(blank=True)
+    password = models.CharField(blank= False, max_length=100)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.password = make_password(self.password)
+        self.firstname = cipher.encrypt(self.firstname.encode('utf-8').decode('utf-8'))
+        self.lastname = cipher.encrypt(self.lastname.encode('utf-8').decode('utf-8'))
+        self.phone = cipher.encrypt(self.phone.encode("utf-8").decode('utf-8'))
+        super(Customer, self).save(*args, **kwargs)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    def get_decrypted_firstname(self):
+        return cipher.decrypt(self.firstname.encode('utf-8')).decode('utf-8')
+
+    def get_decrypted_lastname(self):
+        return cipher.decrypt(self.lastname.encode('utf-8')).decode('utf-8')
+
+    def get_decrypted_phone(self):
+        return cipher.decrypt(self.phone.encode('utf-8')).decode('utf-8')
+
+
+    def __str__(self):
+        if self.has_changed('firstname'):
+            self.firstname = cipher.encrypt(self.firstname.encode('utf-8').decode('utf-8'))
+
+        return f"{self.get_decrypted_firstname()} {self.get_decrypted_lastname()}"
+
+class CustomerToken(models.Model):
+    key = models.CharField(max_length=40, primary_key=True)
+    customer = models.OneToOneField(Customer, related_name="auth_token", on_delete=models.CASCADE)
+    created =models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = binascii.hexlify(os.urandom(20)).decode('utf-8')
+        super().save(self, *args, **kwargs)
+
+    def __str__(self):
+        return self.key
---
+++ rentals/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from customers.models import Customer
+# Create your models here.
+class Location(models.Model):
+
+    Locationid = models.AutoField(primary_key=True)
+    locationname = models.CharField(max_length=50)
+    locationaddress = models.CharField(max_length=50)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=False)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=False)
+    def __str__(self):
+        return self.locationname
+
+class Room(models.Model):
+    Roomid = models.AutoField(primary_key=True)
+    roomname = models.CharField(max_length=50)
+    roomtype = models.CharField(max_length=50)
+    Locationid = models.ForeignKey(Location, on_delete=models.CASCADE)
+    availability = models.BooleanField(default=True)
+    price = models.FloatField(default=0)
+    is_detective_light = models.BooleanField(default=False)
+    is_defective_chair = models.BooleanField(default=False)
+    is_defective_socket = models.BooleanField(default=False)
+    def __str__(self):
+        return f"{self.roomname}+ {self.roomtype}"
+
+class Rental(models.Model):
+    Rentalid = models.AutoField(primary_key=True)
+    Customerid = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    Roomid = models.ForeignKey(Room, on_delete=models.CASCADE)
+    rent_start_time = models.DateTimeField()
+    rent_end_time = models.DateTimeField()
+    charge = models.FloatField(default=0)
+    paymentstatus = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"rent order {self.Rentalid} from {self.rent_start_time} to {self.rent_end_time} at {self.Roomid}"
+
+class Review(models.Model):
+    Reviewid = models.AutoField(primary_key=True)
+    Customerid = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    Roomid = models.ForeignKey(Room, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=5)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.Customerid} {self.Roomid}{self.rating}"
